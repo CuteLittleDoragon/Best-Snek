5416f5df56d4354037c5530c180263d70207a8dd
CuteLittleDoragon <84889740+CuteLittleDoragon@users.noreply.github.com>
2021-07-16 19:12:08 -0700

    Update hello.py

--- welcome/hello.py
+++ welcome/hello.py
@@ -5,17 +5,17 @@ class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-        @commands.Cog.listener()
-        async def on_ready(self):
-            if not self.bot.ready:
-                self.bot.cogs_ready.ready_up("welcome")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("welcome")
                 
-        @commands.Cog.listener()
-        async def on_member_join(self, member, ctx: commands.Context,):
-            await ctx.send(f"Welcome to **{member.guild.name}**!")
+    @commands.Cog.listener()
+    async def on_member_join(self, member, ctx: commands.Context,):
+        await ctx.send(f"Welcome to **{member.guild.name}**!")
             
-        @commands.command()    
-        async def test(self, ctx, echo: str, echo2: str):
-            await ctx.send(echo + " " + echo2)
+    @commands.command()    
+    async def test(self, ctx, echo: str, echo2: str):
+        await ctx.send(echo + " " + echo2)
             
                         
