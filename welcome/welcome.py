dfa46e85101ea354def9a081b5f5de342fb21e1e
CuteLittleDoragon <84889740+CuteLittleDoragon@users.noreply.github.com>
2021-07-16 18:24:01 -0700

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -1,15 +1,15 @@
 import discord
 
-class Welcome(Cog):
+class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-        @Cog.listener()
+        @commands.Cog.listener()
         async def on_ready(self):
             if not self.bot.ready:
                 self.bot.cogs_ready.ready_up("welcome")
                 
-        @Cog.listener()
+        @commands.Cog.listener()
         async def on_member_join(self, member):
             await member.send(f"Welcome to **{member.guild.name}**!")
             
