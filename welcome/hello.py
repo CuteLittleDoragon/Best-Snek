3c3787678b1e2a34b95058c3c981a1f56556cef6
CuteLittleDoragon <84889740+CuteLittleDoragon@users.noreply.github.com>
2021-07-18 15:27:23 -0700

    Update hello.py

--- welcome/hello.py
+++ welcome/hello.py
@@ -11,9 +11,19 @@ class Welcome(commands.Cog):
             self.bot.cogs_ready.ready_up("welcome")
                 
     @commands.Cog.listener()
-    async def on_member_join(member):
-        role = get(member.guild.roles, id=865781081277202533)
-        await member.add_roles(role)
+    async def on_member_join(self, member: discord.Member) -> None:
+        await self.__dm_user(member)
+    
+    
+    
+    
+    async def __dm_user(self, member: discord.Member) -> None:
+        """Sends a DM to the user with a filled-in message_format."""
+
+        message_format = await self.config.guild(member.guild).join.whisper.message()
+
+        try:
+            await member.send(message_format.format(member=member, server=member.guild))
             
     @commands.command()    
     async def test(self, ctx, echo: str, echo2: str):
