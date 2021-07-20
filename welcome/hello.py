c338357fcecbea764ae80dc44589e9508d3ceca3
CuteLittleDoragon <84889740+CuteLittleDoragon@users.noreply.github.com>
2021-07-20 13:59:03 -0700

    Update hello.py

--- welcome/hello.py
+++ welcome/hello.py
@@ -64,7 +64,7 @@ class Welcome(commands.Cog):
         await ctx.send(echo + " " + echo2)
             
     @commands.command()
-    async def my_chance(self, channel: discord.TextChannel):
+    async def my_chance(self, ctx: commands.Context, channel: discord.TextChannel):
         await channel.send("https://cdn.discordapp.com/attachments/763200171743772682/867144101132369960/-AOmundson-_Monster_Musume_no_Iru_Nichijou_OVA_-_02_-1-.1080_1_online-video-cutter.com.mp4")
     
     async def __get_channel(self, guild: discord.Guild) -> discord.TextChannel:
